2deff580d40aab170902c6629231cf94d07610ee
eunsun <nammugmuhg@gmail.com>
2022-05-21 19:35:13 +0900

    Set : Babel 패키지 설치

--- webpack.config.js
+++ webpack.config.js
@@ -23,4 +23,16 @@ module.exports = {
       template: "./public/index.html",
     }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/, // babel-loader로 읽을 파일 확장자 정규표현식
+        exclude: "/node_modules", // 제외할 파일 경로
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/preset-env", "@babel/preset-react"], // babel-loader에서 사용할 옵션
+        },
+      },
+    ],
+  },
 };
