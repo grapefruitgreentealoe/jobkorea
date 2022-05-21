5c8a0f90e2313e17aa63cd2c333cd1c4befb4579
eunsun <nammugmuhg@gmail.com>
2022-05-21 14:49:53 +0900

    Set : webpack 설정

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+// webpack 모듈 설정
+module.exports = () => {
+    mode: 'development', // 개발용:development, 실제:production
+    devtool: 'eval', // 소스맵 관련 옵션
+    resolve: {
+        extensions: ['.js', '.jsx']
+    }, // webpack으로 읽을 파일들의 확장자
+    
+    entry: {
+    	bundle: ['./src/index']
+    }, // 입력
+    output: {
+    	path: path.join(__dirname, 'dist'), // __dirname: 현재 실행 중인 폴더 경로
+        filename: '[name].js'
+    } // 출력
+}
