d7b361334429078bee28b8a703b21659b1517f2f
eunsun <nammugmuhg@gmail.com>
2022-05-27 01:46:34 +0900

    ⛏Function : 개인정보 유효기간 체크 기능

--- src/Join/ui/PersonalInfoSection.jsx
+++ src/Join/ui/PersonalInfoSection.jsx
@@ -1,5 +1,31 @@
 import React from "react";
-import style from "../../../style/join_style/PersonalInfo.module.css";
+import styles from "../../../style/join_style/PersonalInfo.module.css";
+import { useState } from "react";
+import { useEffect } from "react";
 export default function PersonalInfoSection() {
-  return <div className={style.personal}>개인정보 섹션입니다</div>;
+  const period = ["1년", "3년", "회원탈퇴시"];
+  const [periodRadio, setPeriodRadio] = useState(null);
+  const handleRadio = (e) => {
+    setPeriodRadio(e.target.name);
+  };
+
+  useEffect(() => {
+    console.log(periodRadio);
+  }, [periodRadio]);
+  return (
+    <div className={styles.personalSection}>
+      <h3>개인정보 유효기간</h3>
+      {period.map((x) => (
+        <div className={styles.personalInfo}>
+          <input
+            type="radio"
+            name={x}
+            onChange={handleRadio}
+            checked={periodRadio == x}
+          />
+          {x}
+        </div>
+      ))}
+    </div>
+  );
 }
