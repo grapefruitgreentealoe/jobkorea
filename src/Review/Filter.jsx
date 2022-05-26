edab3b1b00182c22624e864e809b8d97dfe29d1e
eunsun <nammugmuhg@gmail.com>
2022-05-27 07:01:02 +0900

    ⛏Function :  합격여부 필터링 기능 추가

--- src/Review/Filter.jsx
+++ src/Review/Filter.jsx
@@ -1,5 +1,21 @@
 import React from "react";
 import styles from "../../style/review_style/Filter.module.css";
-export default function Filter() {
-  return <div className={styles.filter}>필터입니다.</div>;
+export default function Filter({ setFilter }) {
+  const handleFilterButton = (e) => {
+    e.preventDefault();
+    setFilter(e.target.name);
+  };
+  return (
+    <div className={styles.filter}>
+      <button name="전체" onClick={handleFilterButton}>
+        전체
+      </button>
+      <button name="합격" onClick={handleFilterButton}>
+        합격
+      </button>
+      <button name="불합격" onClick={handleFilterButton}>
+        불합격
+      </button>
+    </div>
+  );
 }
